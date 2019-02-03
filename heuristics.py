63e1d1f59a017d9d484306d7df2f6d198f16c3e4
Maria <msymkany@users.noreply.github.com>
2019-02-04 00:09:00 +0200

    manhattan

--- heuristics.py
+++ heuristics.py
@@ -1,1 +1,24 @@
 # class Heuristic():
+from itertools import product
+
+class Manhattan():
+
+    def __init__(self, size):
+        self.dist_map = self.get_distance(size)
+        self.size = size
+        self.len = size * size
+
+    def get_h(self, puzzle, solved):
+        all_h = [self.dist_map[num][solved.index(puzzle[num])]
+                 for num in range(self.len)]
+        return sum(all_h)
+
+    def get_distance_one_num(self, row, col):
+        return [abs(t_row - row) + abs(t_col - col)
+                    for t_row, t_col in product(range(self.size), range(self.size))]
+
+    @staticmethod
+    def get_distance(self):
+        d_map = {((row * self.size) + col) : self.get_distance_one_num(self.size, row, col)
+                 for row, col in product(range(self.size), range(self.size))}
+        return d_map
